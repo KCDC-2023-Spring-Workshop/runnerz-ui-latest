b7b05399af95d7db0ba1f8f947100fd6e4e19e1a
dashaun <dashaun@dashaun.com>
2023-06-21 02:22:17 -0500

    generated using start.vaadin.com

---
+++ src/main/java/dev/dashaun/ui/runnerz/Application.java
@@ -0,0 +1,24 @@
+package dev.dashaun.ui.runnerz;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "runnerz-ui", variant = Lumo.DARK)
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
