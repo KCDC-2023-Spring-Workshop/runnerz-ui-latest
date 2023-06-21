dccf6733ff680c131af15448c6e668d5ee4b07a3
dashaun <dashaun@dashaun.com>
2023-06-21 04:21:37 -0500

    distributed service

--- src/main/java/dev/dashaun/ui/runnerz/Application.java
+++ src/main/java/dev/dashaun/ui/runnerz/Application.java
@@ -5,13 +5,11 @@ import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * The entry point of the Spring Boot application.
- *
+ * <p>
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
- *
  */
 @SpringBootApplication
 @Theme(value = "runnerz-ui", variant = Lumo.DARK)
@@ -20,5 +18,5 @@ public class Application implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    
 }
--- src/main/java/dev/dashaun/ui/runnerz/views/helloworld/HelloWorldView.java
+++ src/main/java/dev/dashaun/ui/runnerz/views/helloworld/HelloWorldView.java
@@ -9,20 +9,24 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import dev.dashaun.ui.runnerz.views.MainLayout;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
 public class HelloWorldView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    private final TextField name;
+    private final Button sayHello;
 
-    public HelloWorldView() {
+    public HelloWorldView(HelloClient helloClient) {
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+            Notification.show(helloClient.hello(name.getValue()));
         });
         sayHello.addClickShortcut(Key.ENTER);
 
@@ -30,6 +34,24 @@ public class HelloWorldView extends HorizontalLayout {
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
 
         add(name, sayHello);
+    }
+
+}
+
+@Service
+class HelloClient {
+    private final RestTemplate restTemplate;
+
+    public HelloClient(RestTemplateBuilder restTemplateBuilder, @Value("${apiBase}") String runnerServiceUri) {
+        this.restTemplate = restTemplateBuilder.rootUri(runnerServiceUri).build();
+    }
+
+    public String hello(String name) {
+        if (name.length() > 0) {
+            return restTemplate.getForObject("/api/hello?name={name}", String.class, name);
+        } else {
+            return restTemplate.getForObject("/api/hello", String.class);
+        }
     }
 
 }
